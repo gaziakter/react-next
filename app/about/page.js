1b05ab33c7e0e6127c62800dd9c75b6b48b52444
Gazi Akter <gaziakterpro@gmail.com>
2025-09-19 15:22:10 +0600

    error handiling

--- app/about/page.js
+++ app/about/page.js
@@ -1,4 +1,6 @@
  export default function About() {
+
+    throw new Error('Failed to fetch data for About page');
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h1>About Us</h1>
